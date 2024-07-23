e7ef2d548d144c50b36d0d91eb50f71578625ad2
yjmantilla <yjmantilla@gmail.com>
2024-07-22 20:29:25 -0500

    refactor: adjust swim speed to 1 and bounds in swim function (to pm 0.2)

--- macrocircuits_parallel.py
+++ macrocircuits_parallel.py
@@ -146,7 +146,7 @@ The environment is flexible, allowing for modifications to introduce additional 
 """
 
 #import dm_control.suite.swimmer as swimmer
-_SWIM_SPEED = 0.1
+_SWIM_SPEED = 1
 
 @swimmer.SUITE.add()
 def swim(
@@ -195,7 +195,8 @@ class Swim(swimmer.Swimmer):
     forward_velocity = -physics.named.data.sensordata['head_vel'][1]
     return rewards.tolerance(
       forward_velocity,
-      bounds=(self._desired_speed, float('inf')),
+      bounds=(self._desired_speed-0.2, self._desired_speed+0.2),
+      #bounds=(self._desired_speed, float('inf')),
       margin=self._desired_speed,
       value_at_margin=0.,
       sigmoid='linear',
